1ef7079a50ce223d3a266a6bc86ae9a6a0e53e15
tbeghin <thomas.beghin@soprasteria.com>
2016-06-03 14:22:48 +0200

    [TBN]

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var planning = require("../src/planning.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var planData = planning.getPlanning();
+    var planData = planning.actualAndNextDeliverer();
     console.log(planData);
     res.render('index', {title : "A qui le tour ?", model : planData});
 });
--- src/planning.js
+++ src/planning.js
@@ -2,12 +2,26 @@
  * Created by Benjamin on 03/06/2016.
  */
 var fs = require('fs');
+var moment = require('moment');
 var planningFile = './data/planning.json';
 
 module.exports = {
-    getPlanning: getPlanning
+    getPlanning: getPlanning,
+    actualAndNextDeliverer : actualAndNextDeliverer
 };
 
-function getPlanning(){
+function getPlanning() {
     return JSON.parse(fs.readFileSync(planningFile, 'utf8'));
+}
+
+function actualAndNextDeliverer() {
+    var planning = getPlanning();
+    var now = moment();
+    var result = [];
+    planning.forEach(
+        function (plan) {
+            console.log(plan.date);
+        }
+    );
+    return planning;
 }
