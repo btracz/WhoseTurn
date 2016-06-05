986cc5d9bf9e2132acefe092482a52722f8dd3a7
Benjamin <benjamin@tracz.fr>
2016-06-05 15:04:04 +0200

    Mailer

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var planning = require("../src/planning.js");
+var mailer = require("../src/mailer");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var planData = planning.actualAndNextDeliverer();
     console.log(planData);
     res.render('index', {title : "A qui le tour ?", model : planData});
+});
+
+router.get('/test-mail', function(req, res, next) {
+    var result = mailer.sendWeeklyNotification();
+    res.send(200, result);
 });
 
 module.exports = router;
---
+++ src/mailer.js
@@ -0,0 +1,65 @@
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+// A mettre en config
+var smtpClient = nodemailer.createTransport(smtpTransport({
+    host: '',
+    port: 25,
+    secure: false, // use SSL
+    auth: {
+        user: "",
+        pass: ""
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+}));
+
+module.exports = {
+    sendWeeklyNotification: sendWeeklyNotification
+};
+
+/**
+ * Envoie la notification de prochain livreur aux abonn�s
+ */
+function sendWeeklyNotification(){
+    return sendMail("b.tracz@club-internet.fr", "Test", "<body>Miam des petits pains !</body>");
+}
+
+/**
+ * Envoie un mail via le serveur mail configur�
+ * @param recipients Destinataire (string) ou tableau des destinataires (array de string)
+ * @param subject
+ * @param body
+ */
+function sendMail(recipients, subject, body){
+    var recipient = '';
+
+    if(Array.isArray(recipients)){
+        recipients.forEach(function(item){
+            recipient += item + ";";
+        });
+        recipient = recipient.slice(0, -1);
+    } else {
+        recipient = recipients;
+    }
+    var mailOpts = {
+        to: recipient,
+        from: "no-reply@",
+        subject: subject,
+        html: body
+    };
+
+    return smtpClient.sendMail(mailOpts, function (error, response) {
+        console.log("Réponse envoi : ");
+        console.log(response);
+        //Email not sent
+        if (error) {
+            // ajout d'une trace dans les logs
+            console.log("Envoi lors de l'envoi du mail '" + subject + "' à " + recipients + ", détails : " + error.toString() + " , stack : " + error.stack());
+            return false;
+        }
+
+        return true;
+    });
+}
