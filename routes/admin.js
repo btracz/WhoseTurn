f72dbf33b06f8259a67699f2476ff5bb21ad2aba
qdecrouy <quentin.decrouy@gmail.com>
2016-06-06 13:11:24 +0200

    [QDY] - Ajout de la fonction authentification

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,33 @@ var express = require('express');
 var userManager = require('../src/users');
 var router = express.Router();
 
+/*Authentification*/
+var basicAuth = require('basic-auth');
+var auth = function (req, res, next) {
+    function unauthorized(res) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        return res.send(401);
+    };
+
+    var user = basicAuth(req);
+
+    if (!user || !user.name || !user.pass) {
+        return unauthorized(res);
+    };
+
+    if (user.name === 'admin' && user.pass === 'password') {
+        return next();
+    } else {
+        return unauthorized(res);
+    };
+};
+
 /* GET users listing. */
-router.get('/', function (req, res) {
+router.get('/', auth, function (req, res) {
     res.send(200, 'Authenticated');
 });
 
-router.get('/users', function (req, res){
+router.get('/users', auth, function (req, res){
     var users = userManager.getUsers();
     var subscribers = userManager.getSubscribers();
     res.status(200).send({ users: users, subscribers : subscribers});
