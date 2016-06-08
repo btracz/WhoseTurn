8c242cb14f5e87cfb5827af8ff10bac4ff4d0d96
Benjamin <benjamin@tracz.fr>
2016-06-08 23:09:48 +0200

    Récupération avatar ok mais à optimiser

--- src/users.js
+++ src/users.js
@@ -3,17 +3,24 @@
  */
 var fs = require('fs');
 var config = require("./config");
+var path = require("path");
 var userFile = './data/users.json';
 
 module.exports = {
     getUsers: getUsers,
     getSubscribers: getSubscribers,
     getSubscribersMails: getSubscribersMails,
-    getUser: getUser
+    getUser: getUser,
+    getAvatar: getAvatar
 };
 
 function getUsers() {
-    return JSON.parse(fs.readFileSync(userFile, 'utf8'));
+    var users = JSON.parse(fs.readFileSync(userFile, 'utf8'));
+    users.forEach(function (user) {
+        user.avatar = getAvatar(user.id);
+    });
+
+    return users;
 }
 
 function getSubscribers() {
@@ -28,12 +35,12 @@ function getSubscribersMails() {
     var mails = [];
     var subscribers = getSubscribers();
 
-    subscribers.forEach(function(item){
-        if(item && item.email){
+    subscribers.forEach(function (item) {
+        if (item && item.email) {
             mails.push(item.email);
-        } else if(item && item.isExternal){
+        } else if (item && item.isExternal) {
             mails.push(config.externalMailFormat().replace("{0}", item.id));
-        } else if(item){
+        } else if (item) {
             mails.push(config.mailFormat().replace("{0}", item.id));
         }
     });
@@ -54,4 +61,19 @@ function getUser(id) {
         console.log('Aucun utilisateur correspondant à ' + id);
         return {"name": id};
     }
+}
+
+function getAvatar(id) {
+    var imagepath = path.join(__dirname, '../public/images/' + id + '.jpg');
+    try {
+        var stats = fs.statSync(imagepath);
+        if (stats.isFile()) {
+            // si le fichier existe le lien est bon
+            return '/images/' + id + '.jpg';
+        }
+    } catch (e) {
+        // fichier inexistant
+    }
+
+    return '/images/inconnu.jpg';
 }
