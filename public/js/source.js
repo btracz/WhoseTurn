9dcb768996e3c6338f7d9c2b6b250f6bbea0be33
tbeghin <thomas.beghin@soprasteria.com>
2016-06-21 14:15:52 +0200

    [TBN] save parameters EC

--- public/js/source.js
+++ public/js/source.js
@@ -1,3 +1,30 @@
-$(window).load(function(){
+$(window).load(function () {
     $('.carousel').carousel('pause');
+});
+
+$("#parametersForm button[type=button]").click(function (event) {
+    var url = "/admin/parameters";
+    var data = {
+        mailServer: {
+            host: $("#host").val(),
+            port: $("#port").val(),
+            secure: $("#secure").prop('checked'),
+            requireTLS: $("#tls").prop('checked'),
+            auth: {
+                user: $("#user").val(),
+                pass: $("#pass").val()
+            }
+        },
+        weeklyNotificationPattern: $('#cron').cron("value")
+    };
+    console.log(data);
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+        }
+    });
 });
